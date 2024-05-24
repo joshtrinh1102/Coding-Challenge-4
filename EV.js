1729b2f6a3d327e6e393540d31f6e5d8656354fc
joshtrinh1102 <nltrinh1102@gmail.com>
2024-05-23 21:56:06 -0400

    Create EV class extending Car with additional charge property.

---
+++ EV.js
@@ -0,0 +1,18 @@
+
+console.log(`U22275330`)// U number
+class Car{
+        constructor(make, speed){
+            this.make = make;
+            this.speed = speed;
+            console.log(`Make: ${make}`)
+            console.log(`Speed: ${speed}`)
+        }
+        
+        calculateacceleration(){
+            return this.speed + 10 }
+    
+        calculatebrake(){
+            return this.brake -5
+        }
+
+        }
